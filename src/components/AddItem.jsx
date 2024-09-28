c86cb7773ef8113407496712179416c1547b0df8
Ganesh Vhanmane <mr.vga1415@gmail.com>
2024-09-28 07:50:43 +0530

    chnage state like a object in addItem

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -3,34 +3,46 @@ import { CrudContext } from "../context/CrudContext";
 
 const AddItem = () => {
   const { addItem } = useContext(CrudContext);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [item, setItem] = useState({
+    name: "",
+    email: "",
+  });
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  const handleChange = (e) => {
+    setItem({ ...item, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newData = {
       id: Date.now(),
-      name,
-      email,
+      name: item.name,
+      email: item.email,
     };
     // console.log(newData);
     addItem(newData);
-    setEmail("");
-    setName("");
+    // setEmail("");
+    // setName("");
+    setItem({ name: "", email: "" });
   };
   return (
     <form onSubmit={handleSubmit} className="p-4">
       <input
         type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={item.name}
+        name="name"
+        // onChange={(e) => setName(e.target.value)}
+        onChange={handleChange}
         className="border p-2 rounded mr-2"
         placeholder="Item name"
       />
       <input
         type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        name="email"
+        value={item.email}
+        // onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => handleChange(e)}
         className="border p-2 rounded mr-2 "
         placeholder="Item email"
       />
